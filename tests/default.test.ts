2a4ce4557c552c389ca2aad5829102bb1c45e96e
Michael Geyer <michael@qbitone.de>
2022-10-03 16:42:16 +0200

    chore: rename test folder to tests

--- tests/default.test.ts
+++ tests/default.test.ts
@@ -2,13 +2,20 @@ import { useGeneratePassword } from '../src/composables/useGeneratePassword';
 import { describe, test, expect } from 'vitest';
 
 describe('Generate Password', () => {
-  test('Hello, World!', () => {
+  test('Simple test', () => {
     const sentence = 'Hello, World!';
     expect(useGeneratePassword(sentence)).toBe('H,W!');
   });
 
-  test('Fuchs du hast die Gans gestohlen, gib sie wieder her!', () => {
+  test('Password generated using simple sentence.', () => {
     const sentence = 'Fuchs du hast die Gans gestohlen, gib sie wieder her!';
     expect(useGeneratePassword(sentence)).toBe('FdhdGg,gswh!');
+  });
+
+  test('Password generated using simple sentence and numbers', () => {
+    const sentence = 'Fuchs du hast die Gans gestohlen, gib sie wieder her!';
+    expect(useGeneratePassword(sentence, { useNumbers: true })).toBe(
+      'Fdhd99,9swh!'
+    );
   });
 });
