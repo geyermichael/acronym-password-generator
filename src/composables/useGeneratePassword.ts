77e928e37ec755dfd2c6d6e5e26f07e7288d5c69
Michael Geyer <michael@qbitone.de>
2022-10-03 15:08:52 +0200

    feat: implement simple password generation

--- src/composables/useGeneratePassword.ts
+++ src/composables/useGeneratePassword.ts
@@ -1,4 +1,18 @@
-export function useGeneratePassword(sentence: string) {
-  sentence = sentence.replaceAll('\n', ' ');
-  return 'HW';
+const specialCharacters: Set<string> = new Set(['.', ',', '!']);
+
+export function useGeneratePassword(sentence: string): string {
+  if (sentence == '') throw new Error('Empty string is not allowed.');
+
+  sentence = sentence.replaceAll('\n', ' ').trim(); // remove all line breaks and trim sentence
+  let words: string[] = sentence.split(' ').filter((word) => word != ''); // create words array and remove all spaces
+
+  let password: string[] = [];
+  words.forEach((word) => {
+    password.push(word[0]);
+    if (specialCharacters.has(word[word.length - 1])) {
+      password.push(word[word.length - 1]);
+    }
+  });
+
+  return password.join(''); // return password as string;
 }
