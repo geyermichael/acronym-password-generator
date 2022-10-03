5c54b832cbaf986546727df1ab3448bc9343d0c9
Michael Geyer <michael@qbitone.de>
2022-10-03 15:09:14 +0200

    chore: ui improvments

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js"
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+    './node_modules/flowbite/**/*.js',
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('flowbite/plugin')
-]
-}
+  darkMode: 'media',
+  plugins: [require('flowbite/plugin')],
+};
